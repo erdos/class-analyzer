9459d475e4b491ffcf225c47d0fbccafb4204036
janos erdos <erdosj@gmail.com>
2019-08-24 23:27:17 +0200

    code attribute reading

--- src/class_analyzer/code.clj
+++ src/class_analyzer/code.clj
@@ -1,34 +1,50 @@
 (ns class-analyzer.code
   (:import [java.io ByteArrayInputStream InputStream DataInputStream])
   (:require [class-analyzer.opcodes :refer :all]
-            [class-analyzer.util :refer :all]))
+            [class-analyzer.util :refer :all]
+            [class-analyzer.core :as core]))
+
+(set! *warn-on-reflection* true)
 
 (def ^:private ^:dynamic ^java.io.DataInputStream *input-stream*)
 (def ^:private ^:dynamic *byte-offset*)
+(def ^:private ^:dynamic *constant-pool*)
 
 (defn- read-byte []
-  (println "Readin byte")
+  (swap! *byte-offset* inc)
   (.readByte *input-stream*))
 
+(defn- read-unsigned-byte []
+  (swap! *byte-offset* inc)
+  (.readUnsignedByte *input-stream*))
+
 (defn- read-short []
-  (println "Reading short")
+  (swap! *byte-offset* + 2)
   (.readShort *input-stream*))
 
+(defn- read-unsigned-short []
+  (swap! *byte-offset* + 2)
+  (.readUnsignedShort *input-stream*))
+
 (defn- read-int []
-  (println "Reading int")
+  (swap! *byte-offset* + 4)
   (.readInt *input-stream*))
+
+#_
+(defn- read-unsigned-int []
+  (swap! *byte-offset* + 4)
+  (.readUnsignedInt *input-stream*))
 
 (defmulti ^:pivate read-op (fn [instruction-map] (:mnemonic instruction-map)))
 
 
-
 (defn read-arg [a]
   (case a
-    :cpidx2 (read-short) ;; index in the constant pool
-    :cpidx1 (read-byte)  ;; index in the constant pool
-    :branchoffset (read-short) ;; branch offset 2 bytes
-    :branchoffset4 (read-int) ;; 4 byte branch offset
-    :zerobyte (doto (read-byte) (-> zero (assert "Expected zero byte!")))
+    :cpidx2 (read-unsigned-short) ;; index in the constant pool
+    :cpidx1 (read-unsigned-byte)  ;; index in the constant pool
+    :branchoffset (read-unsigned-short) ;; branch offset 2 bytes
+    :branchoffset4 (read-int) ;; 4 byte branch offset - maybe unsignded?
+    :zerobyte (doto (read-byte) (-> zero? (assert "Expected zero byte!")))
     :byte (read-byte)
     :int (read-int)
     :short (read-short)))
@@ -38,6 +54,7 @@
   {:args (mapv read-arg args)})
 
 (defmethod read-op :tableswitch [_]
+  (assert false "Not impled!")
   (dotimes [_ (- 4 (mod @*byte-offset* 4))] (read-byte))
   (let [low     (read-int)
         high    (read-int)
@@ -48,25 +65,21 @@
      :default default
      :offsets offsets}))
 
-(defn- read-code [^InputStream input-stream]
-  (let [[counter istream] (wrap-counted input-stream)]
-    (binding [*byte-offset* counter
-              *input-stream* (new java.io.DataInputStream istream)]
-      (let [code-length      (.readInt input-stream)]
-        (println "Code length is " code-length)
-        (reset! *byte-offset* 0)
-        (doall
-         (for [i (range)
-               :let [offset @*byte-offset*
-                     _ (println :? offset)]
-               :while (< offset code-length)
-               :let [opcode (read-byte)
-                     instruction-map (get instructions opcode)]]
-           (assoc (read-op instruction-map)
-                  :op-code opcode
-                  :mnemonic (:mnemonic instruction-map)
-                  :offset offset
-                  :nr     i)))))))
+(defn- read-code [^InputStream istream]
+  (binding [*byte-offset* (atom 0)
+            *input-stream* (new java.io.DataInputStream istream)]
+    (let [code-length      (.readInt *input-stream*)]
+      (doall
+       (for [i (range)
+             :let [offset @*byte-offset*]
+             :while (< offset code-length)
+             :let [opcode (read-unsigned-byte)
+                   instruction-map (get instructions opcode)]]
+         (assoc (read-op instruction-map)
+                :op-code opcode
+                :mnemonic (:mnemonic instruction-map)
+                :offset offset
+                :nr     i))))))
 
 (->
  [
@@ -93,14 +106,35 @@
       :catch-type (read-short) ;; class info in constant pool
       })))
 
-                                        ;(declare read-attributes)
-(defn read-attributes [])
 
-(defn read-code-attribute [input-stream]
-  (binding [*byte-offset* (atom 0)
-            *input-stream* input-stream]
+(defn read-attributes []
+  ;; TODO: discriminator is either method or class?
+  (core/read-attributes :method *input-stream* *constant-pool*))
+
+(defn read-code-attribute []
+  (binding [*byte-offset* (atom 0)]
     {:max-stack       (read-short)
      :max-locals      (read-short)
-     :code            (read-code)
+     :code            (read-code *input-stream*)
      :exception-table (read-exception-table)
      :attrs           (read-attributes)}))
+
+(defmethod core/read-attribute "Code" [_ ^DataInputStream dis _ _ constant-pool]
+  (binding [*constant-pool* constant-pool
+            *input-stream* dis]
+    (read-code-attribute)))
+
+(defmethod core/read-attribute "LineNumberTable" [_ ^DataInputStream dis _ _ constant-pool]
+  (doall
+   (for [i (range (.readUnsignedShort dis))]
+     {:start-pc    (.readUnsignedShort dis)
+      :line-number (.readUnsignedShort dis)})))
+
+(defmethod core/read-attribute "LocalVariableTable" [_ ^DataInputStream dis _ _ constant-pool]
+  (doall
+   (for [i (range (.readUnsignedShort dis))]
+     {:start-pc    (.readUnsignedShort dis)
+      :length      (.readUnsignedShort dis)
+      :name-idx    (-> dis .readUnsignedShort constant-pool :data str!)
+      :descr-idx   (-> dis .readUnsignedShort constant-pool :data str!)
+      :index       (.readUnsignedShort dis)})))
